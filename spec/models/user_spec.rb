8e7aaac39ac9bff1c3d32f100680bb8816c226c8
Yechiel K <chilikkal@gmail.com>
2017-04-24 22:05:13 -0400

    update user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,10 +4,15 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it 'requires a username and a password upon creation'
     it 'requires that the username be unique'
+
+  end
+
+  describe 'on save' do
+    it 'hashes a password'
   end
 
   describe 'relationships' do
     it 'has many applications'
   end
-  
+
 end
