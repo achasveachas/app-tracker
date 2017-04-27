722b86ccfa053dbc5f553b28689557e02f14ba6c
Yechiel K <chilikkal@gmail.com>
2017-04-27 00:35:51 -0400

    loaded lib in application

--- config/application.rb
+++ config/application.rb
@@ -18,13 +18,7 @@ Bundler.require(*Rails.groups)
 
 module AppTracker
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
+    config.autoload_paths << Rails.root.join('lib')
     config.api_only = true
   end
 end
