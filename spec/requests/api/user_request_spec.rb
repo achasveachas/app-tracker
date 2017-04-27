a7b524e10d066462af746cf92b3006d1b934ea2a
Yechiel K <chilikkal@gmail.com>
2017-04-26 23:37:02 -0400

    add spec for api users

--- spec/requests/api/user_request_spec.rb
+++ spec/requests/api/user_request_spec.rb
@@ -6,7 +6,27 @@ RSpec.describe "API::V1::Users", type: :request do
 
     describe "on success" do
 
-      it "creates a user from the params"
+      it "creates a user from the params" do
+        params = {
+          user: {
+            username: "testuser",
+            password: "testtest"
+          }
+        }
+
+        post "/api/v1/user",
+          params: params.to_json,
+          headers: { 'Content-Type': 'application/json'}
+
+        body = JSON.parse(response.body)
+
+        expect(response.status).to eq(200)
+        expect(body['token']).not_to eq(nil)
+        expect(body['user']['id']).not_to eq(nil)
+        expect(body['user']['username']).to eq("testuser")
+        expect(body['user']['password_digest']).to eq(nil)
+
+      end
 
       it "returns the new user and a JWT token"
     end
