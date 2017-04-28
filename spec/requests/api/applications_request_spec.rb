d5cf1780a21448ce59fa1ec2c2e3729e3fcc5bd6
Yechiel K <chilikkal@gmail.com>
2017-04-27 22:19:40 -0400

    set up before_each for application routes specs

--- spec/requests/api/applications_request_spec.rb
+++ spec/requests/api/applications_request_spec.rb
@@ -5,6 +5,14 @@ RSpec.describe "API::V1::Applications", type: :request do
   befor(:each) do
     @user = create(:user)
     @token = Auth.create_token(@user.id)
+    @token_headers = {
+      'Content-Type': 'application/json'
+    }
+    @tokenless_headers = {
+      'Accept': 'application/json'
+      'Content-Type': 'application/json',
+      'Authorization': "Bearer: #{@token}"
+    }
   end
 
   it "requires all routes to have a token" do
