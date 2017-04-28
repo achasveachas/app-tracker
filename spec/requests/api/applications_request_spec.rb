ef71f8f47085536154c42c9e484d0aea020c2f54
Yechiel K <chilikkal@gmail.com>
2017-04-28 15:08:35 -0400

    added specs to make sure only authorized users can add or edit aplications

--- spec/requests/api/applications_request_spec.rb
+++ spec/requests/api/applications_request_spec.rb
@@ -4,18 +4,30 @@ RSpec.describe "API::V1::Applications", type: :request do
 
   befor(:each) do
     @user = create(:user)
+    @application = @user.applications.create(company: "Google")
     @token = Auth.create_token(@user.id)
     @token_headers = {
       'Content-Type': 'application/json'
     }
     @tokenless_headers = {
-      'Accept': 'application/json'
       'Content-Type': 'application/json',
-      'Authorization': "Bearer: #{@token}"
     }
   end
 
-  it "requires all routes to have a token" do
+  it "requires a token to make and edit an application" do
+    responses = []
+    response_bodies = []
+
+    post "/api/v1/users/#{@user.id}/applications", params: {comapny: "Yahoo"}, headers: @tokenless_headers
+    responses << response
+    response_bodies << JSON.parse(response.body)
+
+    patch "/api/v1/users/#{@user.id}/applications/#{@application.id}", params: {comapny: "Yahoo"}, headers: @tokenless_headers
+    responses << response
+    response_bodies << JSON.parse(response.body)
+
+    responses.each {|r| expect(r).to have_http_status(403)}
+    response_bodies.each {|b| expect(b["errors"].to eq([{"message" => "Token is invalid"}]))}
   end
 
 end
