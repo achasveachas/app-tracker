20c9e2e1f5a7dc902c0c7df452582242e6e09b1c
Yechiel K <chilikkal@gmail.com>
2017-04-30 11:06:47 -0400

    added spec for GET /applications

--- spec/requests/api/applications_request_spec.rb
+++ spec/requests/api/applications_request_spec.rb
@@ -2,33 +2,64 @@ require 'rails_helper'
 
 RSpec.describe "API::V1::Applications", type: :request do
 
-  before(:each) do
-    @user = create(:user)
-    @application = @user.applications.create(company: "Google")
-    @token = Auth.create_token(@user.id)
-    @token_headers = {
-      'Content-Type': 'application/json'
-    }
-    @tokenless_headers = {
-      'Content-Type': 'application/json',
-    }
+  describe "authentication" do
+
+    before(:each) do
+      @user = create(:user)
+      @application = @user.applications.create(company: Faker::Company.name)
+      @token = Auth.create_token(@user.id)
+      @token_headers = {
+        'Content-Type': 'application/json'
+      }
+      @tokenless_headers = {
+        'Content-Type': 'application/json',
+      }
+    end
+
+    it "requires a token to make and edit an application" do
+      responses = []
+      response_bodies = []
+
+      post "/api/v1/users/#{@user.id}/applications", headers: @tokenless_headers
+      responses << response
+      response_bodies << JSON.parse(response.body)
+
+
+      patch "/api/v1/users/#{@user.id}/applications/#{@application.id}", headers: @tokenless_headers
+      responses << response
+      response_bodies << JSON.parse(response.body)
+
+      responses.each {|r| expect(r).to have_http_status(403)}
+      response_bodies.each {|body| expect(body["errors"]).to eq([{"message" => "You must include a JWT token"}])}
+    end
+
   end
 
-  it "requires a token to make and edit an application" do
-    responses = []
-    response_bodies = []
+  describe "routes" do
 
-    post "/api/v1/users/#{@user.id}/applications", params: {}, headers: @tokenless_headers
-    responses << response
-    response_bodies << JSON.parse(response.body)
+    before(:each) do
+      @user = create(:user)
+      3.times do
+        @user.applications.create(company: Faker::Company.name)
+      end
+    end
 
+    describe "GET /api/v1/users/:id/applications" do
 
-    patch "/api/v1/users/#{@user.id}/applications/#{@application.id}", params: {}, headers: @tokenless_headers
-    responses << response
-    response_bodies << JSON.parse(response.body)
+      it "returns an array of applications belonging to a given user" do
 
-    responses.each {|r| expect(r).to have_http_status(403)}
-    response_bodies.each {|body| expect(body["errors"]).to eq([{"message" => "You must include a JWT token"}])}
+        get "/api/v1/users/#{@user.id}/applications"
+
+        body = JSON.parse(response.body)
+
+        expect(response.status).to eq(200)
+        expect(body['applications']).to be_an(Array)
+        expect(body['applications'].size).to eq(3)
+      end
+
+      it "doesn't return applications belonging to other users"
+    end
+
   end
 
 end
