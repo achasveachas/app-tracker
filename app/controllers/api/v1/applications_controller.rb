d21922bab3d3b79a4b467b2c33269dd26f611d83
Yechiel K <chilikkal@gmail.com>
2017-04-30 11:34:43 -0400

    added validation for bed applications index request

--- app/controllers/api/v1/applications_controller.rb
+++ app/controllers/api/v1/applications_controller.rb
@@ -2,8 +2,16 @@ class Api::V1::ApplicationsController < ApplicationController
   before_action :authenticate_token!, only: [:create, :update]
 
   def index
-    @applications = User.find_by(id: params[:user_id]).applications.sort
-    render 'users/applications.json.jbuilder', applications: @applications
+    @applications = User.find_by(id: params[:user_id])&.applications&.sort
+    if @applications
+      render 'users/applications.json.jbuilder', applications: @applications
+    else
+      render json: {
+        errors: [
+          {message: "Page not found"}
+        ]
+      }, status: 404
+    end
   end
 
   def create
