596b7544c15aaa466d91e605a7d0881bfab9cf9c
Yechiel K <chilikkal@gmail.com>
2017-05-10 20:11:12 -0400

    added the front-end to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'http://localhost:3000', 'https://app-tracker-react.herokuapp.com'
 
     resource '*',
       headers: :any,
