a9d5307c006197c10eee56a6ea023b9969dad171
Yechiel K <chilikkal@gmail.com>
2017-05-12 16:07:26 -0400

    updated users controller to take name into account

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -27,6 +27,6 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:name, :username, :password)
   end
 end
