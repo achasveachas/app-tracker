decb6468ce8475d87daef3e08c17e9f11eb8e177
Yechiel K <chilikkal@gmail.com>
2017-05-14 00:30:26 -0400

    added patch to routes.rb to try and fix a cors issue

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
 
     end
   end
+  # global options responder -> makes sure OPTION request for CORS endpoints work
+  match '*path', via: [:options], to: lambda {|_| [204, { 'Content-Type' => 'text/plain' }]}
 
 end
