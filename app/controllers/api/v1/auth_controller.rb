94249286695cedd41ca4ae50cd7cd137ab6e3314
Yechiel K <chilikkal@gmail.com>
2017-05-14 00:13:38 -0400

    changed the error messages on authentication to something the client can interpret

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -5,16 +5,13 @@ class Api::V1::AuthController < ApplicationController
     @user = User.find_by(username: params[:user][:username])
     if !@user
       render json: {
-        errors: {
-          username: ["Unable to find user with that username"]
-        }
+        errors: "Unable to find user with that username"
       }, status: 500
     elsif @user&.authenticate(params[:user][:password])
       render 'users/user_with_token.json.jbuilder', user: @user
     else
       render json: {
-        errors: {
-          password: ["Password does not match"]
+        errors: "Password does not match"
         }
       }, status: 500
     end
