344f06e388df30c3719ff5e1fb921015d9dceb2e
Yechiel K <chilikkal@gmail.com>
2017-05-14 00:18:36 -0400

    updated cors settings to allow requests from all origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'https://app-tracker-react.herokuapp.com'
+    origins '*'
 
     resource '*',
       headers: :any,
