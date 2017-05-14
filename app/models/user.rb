612c4191b6cd7bfb7352ad8394ab72f625ea2d67
Yechiel K <chilikkal@gmail.com>
2017-05-13 23:36:06 -0400

    added code to User model to capitalize the name

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
   validates :username, presence: true
   validates :username, uniqueness: true
   validates :password, length: {minimum: 8}
+
+  before_validation do
+    self.name = self.name.capitalize if self.name
+  end
 end
