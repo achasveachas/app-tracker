a163e84b7180fdd7c741530e6db64ee245320887
Yechiel K <chilikkal@gmail.com>
2017-05-15 19:32:18 -0400

    updated the controller not to return a 404 if a user has 0 applications

--- app/controllers/api/v1/applications_controller.rb
+++ app/controllers/api/v1/applications_controller.rb
@@ -2,16 +2,10 @@ class Api::V1::ApplicationsController < ApplicationController
   before_action :authenticate_token!, only: [:create, :update]
 
   def index
+
     @applications = User.find_by(id: params[:user_id])&.applications&.sort
-    if @applications.size > 0
-      render 'applications/applications.json.jbuilder', applications: @applications
-    else
-      render json: {
-        errors: [
-          {message: "Page not found"}
-        ]
-      }, status: 404
-    end
+    render 'applications/applications.json.jbuilder', applications: @applications
+    
   end
 
   def show
